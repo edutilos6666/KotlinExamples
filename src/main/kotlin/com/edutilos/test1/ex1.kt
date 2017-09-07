6602e5d459260781e951e11c31bab41f8b55c1e6
edutilos666 <edutilosaghayev@gmail.com>
2017-09-07 12:11:36 +0200

    second

---
+++ src/main/kotlin/com/edutilos/test1/ex1.kt
@@ -0,0 +1,11 @@
+package com.edutilos.test1
+
+
+
+data class Person(val name:String , val age: Int? = null)
+
+fun main(args:Array<String>) {
+    val persons = listOf(Person("Alice"), Person("Bob", age = 29))
+    val oldest = persons.maxBy {it.age ?: 0}
+    println("The oldest is: $oldest")
+}
